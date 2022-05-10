18cc789a3a41e9c6ff6f94fd8a3c5cbecb71fdc8
michealuzer <michealuzer@gmail.com>
2022-05-10 14:24:35 +0300

    finished gmail config .gmail lesssecureapps .unlockCaptcha

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,8 @@ from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 from .models import Post
 from django.contrib import messages
+from django.conf import settings
 from django.views.generic import DetailView
-
 # Create your views here.
 
 def home(request):
@@ -15,16 +15,18 @@ def home(request):
         phone = request.POST.get('Phone')
         email = request.POST.get('Email')
         message = request.POST.get('Message')
+        subject = 'new member, contact info'
 
         data = {
             'name': name,
             'phone': phone,
             'email': email,
             'message': message,
+
         }
 
-        message = "the message is:\n{data['message']}\nfrom: {data['name']} with email: {data['email']}\nhis/her number is: {data['phone']}"
-        # send_mail('contact',message,'',['michealuzer@gmail.com'])
+        message = f"the message is:\n{data['message']}\nfrom: {data['name']} with email: {data['email']}\nhis/her number is: {data['phone']}"
+        send_mail(subject,message,settings.EMAIL_HOST_USER,['michealuzer@gmail.com',])
         messages.success(request, 'thank you for getting in touch with us' )
 
         return redirect('home')
